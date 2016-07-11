a596768850cdc809670a4302efc95b7006834e9d
王佳 <wangjia_1919@163.com>
2016-07-11 11:35:47 +0800

    fix a test bug

--- src/main/java/com/lollipop/controller/TestController.java
+++ src/main/java/com/lollipop/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.lollipop.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,18 +14,23 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/")
 public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping("")
-    public String home(){
+    public String home() {
         return "hello";
     }
 
     @RequestMapping("/getTid")
-    public ModelAndView getTid(){
+    public ModelAndView getTid() {
         GetPaper getPaper = new GetPaper();
         String tid = getPaper.getTid();
         ModelMap result = new ModelMap();
 
-        result.put("tid",tid);
-        return new ModelAndView("index",result);
+
+
+        result.put("tid", tid);
+        logger.info("getTid={}",result.get("tid"));
+        return new ModelAndView("index", result);
     }
 }
--- src/main/resource/templates/index.ftl
+++ src/main/resource/templates/index.ftl
@@ -3,6 +3,6 @@
     <title>测试页面</title>
 </head>
 <body>
-    ${result.tid.tid}
+${tid}
 </body>
 </html>
